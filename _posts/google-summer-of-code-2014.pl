a86fe2dc37c1dbc51a22dc5d79eb84da6e8f0817
Henrik Brautaset Aronsen <subsurface@henrik.synth.no>
2017-01-04 20:22:45 +0100

    Replace trac links with GitHub issue links
    Signed-off-by: Henrik Brautaset Aronsen <subsurface@henrik.synth.no>

--- _posts/google-summer-of-code-2014.pl
+++ _posts/google-summer-of-code-2014.pl
@@ -10,4 +10,4 @@ permalink: >
 published: true
 ---
 <a href="http://www.google-melange.com/gsoc/org2/google/gsoc2014/subsurface"><img class="aligncenter size-medium wp-image-983" alt="GSoC-2014" src="https://subsurface-divelog.org/wp-content/uploads/2014/02/GoogleSummer_2014logo_avatar-300x300.png" width="300" height="300" /></a>
-Po raz pierwszy Subsurface został zaakceptowany w GSoC2014! Sprawdź nasza <a href="http://trac.subsurface-divelog.org/wiki/Subsurface_GSOC_2014_Idea_List">Listę Pomysłów</a> lub zgłoś własną propozycję. Studenci - czekamy na Was!
+Po raz pierwszy Subsurface został zaakceptowany w GSoC2014! Sprawdź nasza <a href="https://github.com/Subsurface-divelog/subsurface/issues/wiki/Subsurface_GSOC_2014_Idea_List">Listę Pomysłów</a> lub zgłoś własną propozycję. Studenci - czekamy na Was!
